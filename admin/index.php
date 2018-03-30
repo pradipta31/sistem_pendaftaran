2bd4d23d81e55d3a9897dbab34ae14c99737af4d
pradipta31 <pradiptadipta31@gmail.com>
2018-03-30 15:02:09 +0800

    pendaftaran fixed it

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,7 @@ $nama = ( isset($_SESSION['nama_user']) ) ? $_SESSION['nama_user'] : '';
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Dashboard</title>
+  <title>DISNAKER Provinsi Bali | Dashboard</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="../bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" href="../bower_components/font-awesome/css/font-awesome.min.css">
