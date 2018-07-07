7e454f9e6e1b46cc073110c03fdda9aa8c7da6d0
Dipta Pradipta <pradiptadipta31@gmail.com>
2018-07-07 23:13:53 +0800

    njing

--- peserta/pendaftaran.php
+++ peserta/pendaftaran.php
@@ -17,7 +17,6 @@ include "kiripeserta.php";
                                       include  "koneksi.php";
                                       $query = mysqli_query($koneksi,'SELECT nomor_peserta FROM peserta order by id DESC LIMIT 1'); // menampilkan data nomor peserta pada tabel peserta dengan mengurutkan id dari yang terbawah
                                       $a = mysqli_fetch_assoc($query); // menampilkan data nomor peserta
-
                                       $b = substr($a['nomor_peserta'],5,8); // memotong string dan mengambil nomor peserta yaitu urutan 5 dan 8
                                       $c = $b+1; // nomor peserta akan bertambah jika terdapat data sebelumnya
                                       $d = "00".$c; // untuk menggabungkan tahun
