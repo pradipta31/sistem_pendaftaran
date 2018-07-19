4f4b1b653887a3bce82e533534733c0cd6042d24
wahyu357 <cahyaniwahyu7@gmail.com>
2018-07-19 14:08:04 -0700

    c

--- kepalakantor/laporan-peserta.php
+++ kepalakantor/laporan-peserta.php
@@ -1,42 +1,73 @@
 <?php
-  include "kiri.php";
-  include "koneksi.php";
+include "kiri.php";
+include "koneksi.php";
 
-?>
-<html>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
-           <script type="text/javascript">
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
- 
-function drawChart() {
- 
-    var data = google.visualization.arrayToDataTable([
-      ['Language', 'Rating'],
-      <?php
-      if($result->num_rows > 0){
-          while($row = $result->fetch_assoc()){
-            echo "['".$row['name']."', ".$row['rating']."],";
-          }
-      }
-      ?>
-    ]);
-    
-    var options = {
-        title: 'Most Popular Social Media',
-        width: 900,
-        height: 500,
-    };
-    
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-    
-    chart.draw(data, options);
-}
-</script>
-  <body>
 
-  </body>
+
+$tahun = $koneksi->query("SELECT * FROM peserta WHERE tahun =''");
+$denpasar = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Denpasar'");
+$badung = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Badung'");
+$negara = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Negara'");
+$karangasem = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Karangasem'");
+$bangli = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Bangli'");
+$buleleng = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Buleleng'");
+$tabanan = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Tabanan'");
+$klungkung = $koneksi->query("SELECT * FROM peserta WHERE kabupaten ='Klungkung'");
+
+
+$jumlah_gianyar= mysqli_num_rows($gianyar);
+$jumlah_denpasar= mysqli_num_rows($denpasar);
+$jumlah_badung= mysqli_num_rows($badung);
+$jumlah_negara= mysqli_num_rows($negara);
+$jumlah_karangasem= mysqli_num_rows($karangasem);
+$jumlah_bangli= mysqli_num_rows($bangli);
+$jumlah_buleleng= mysqli_num_rows($buleleng);
+$jumlah_tabanan= mysqli_num_rows($tabanan);
+$jumlah_klungkung= mysqli_num_rows($klungkung);
+
+ ?>
+
+ <html>
+   <head>
+     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+   <script type="text/javascript">
+     google.charts.load("current", {packages:['corechart']});
+     google.charts.setOnLoadCallback(drawChart);
+     function drawChart() {
+       var data = google.visualization.arrayToDataTable([
+         ["Element", "Jumlah", { role: "style" } ],
+         ['Gianyar',     <?php echo $jumlah_gianyar;?>, 'color: gray'],
+         ['Denpasar',      <?php echo $jumlah_denpasar;?>, 'color: #76A7FA'],
+         ['Badung',     <?php echo $jumlah_badung;?>, 'color: black'],
+         ['Negara',      <?php echo $jumlah_negara;?>,'color: yellow' ],
+         ['Karangasem',     <?php echo $jumlah_karangasem;?>, 'color: black'],
+         ['Bangli',      <?php echo $jumlah_bangli;?>, 'color: black'],
+         ['Buleleng',     <?php echo $jumlah_buleleng;?>, 'color: black'],
+         ['Tabanan',      <?php echo $jumlah_tabanan;?>, 'color: black'],
+         ['Klungkung',     <?php echo $jumlah_klungkung;?>, 'color: black'],
+       ]);
+
+       var view = new google.visualization.DataView(data);
+       view.setColumns([0, 1,
+                        { calc: "stringify",
+                          sourceColumn: 1,
+                          type: "string",
+                          role: "annotation" },
+                        2]);
+
+       var options = {
+         title: "",
+         width: 1000,
+         height: 500,
+         bar: {groupWidth: "95%"},
+         legend: { position: "none" },
+       };
+       var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+       chart.draw(view, options);
+   }
+   </script>
 </html>
+
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper" style="background-color: white">
   <!-- Content Header (Page header) -->
@@ -57,23 +88,30 @@ function drawChart() {
                 }
                 ?>
               </select>
+
               <?php
-                echo $result['tgl_pendaftaran'];
-                echo $tahun18;
-                print_r($result);
+              $fff = mysqli_query($koneksi, "SELECT COUNT(*),YEAR(tgl_pendaftaran) FROM peserta GROUP BY YEAR(tgl_pendaftaran)");
+              $asu = mysqli_fetch_assoc($fff);
+              print_r($asu);
               ?>
+
             </div>
           </div>
         </div>
       </div>
-
+      <div class="col-md-2">
+      </div>
   </div>
-
   <section class="content">
-    <div id="piechart"></div>
+    <div class="row">
+      <!-- <div id="columnchart_values" style="width: 100%; height: 640px; margin-left: 10px; margin-top: 30px">
+      </div> -->
+
+      <a href="#" onclick="window.print()"> Print </a>
+
+    </div>
   </section>
 </div>
-
 <footer class="main-footer">
 <div class="pull-right hidden-xs">
   <b>Version</b> 1.0.0
@@ -81,4 +119,3 @@ function drawChart() {
 <strong>Copyright &copy; 2016-2017 <a href="#">DISNAKER Prov. Bali</a>.</strong> All rights
 reserved.
 </footer>
-
