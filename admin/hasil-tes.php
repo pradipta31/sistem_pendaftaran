352fbdf8eecb538dad0afc81233918d9e69423d9
Dipta Pradipta <pradiptadipta31@gmail.com>
2018-07-19 14:06:50 +0800

    push yang

--- admin/hasil-tes.php
+++ admin/hasil-tes.php
@@ -12,44 +12,6 @@ include "kiri.php";
   $statement->execute();
 
   $result = $statement->fetchAll();
-  // function fill_Peserta($koneksi){
-  //   $output = '';
-  //   $sql = "SELECT * FROM peserta";
-  //   $hasil = mysqli_query($koneksi,$sql);
-  //   while ($rowss = mysqli_fetch_assoc($hasil)) {
-  //     $tanggal = $rowss['tgl_pendaftaran'];
-  //     $tahun_extract = date("Y",strtotime($tanggal));
-  //     $output .= '<option value="'.$rowss["id_peserta"].'">'.$tahun_extract.'</option>';
-  //   }
-  //   return $output;
-  // }
-  //
-  // function fill_hasilTes($koneksi){
-  //   $output = '';
-  //   $sql = "SELECT * FROM hasil_tes";
-  //   $hasil = mysqli_query($koneksi,$sql);
-  //   $nomor = 1;
-  //   $output .= '<table class="table table-bordered">';
-  //   $output .= '<tr>';
-  //   $output .= '<th style="width: 20px"><center>No</center></th>';
-  //   $output .= '<th><center>Nomor Peserta</center></th>
-  //               <th><center>Nama Peserta</center></th>
-  //               <th><center>Total Nilai</center></th>
-  //               <th><center>Status</center></th>
-  //               <th><center>Opsi</center></th>';
-  //   while ($rowss = mysqli_fetch_assoc($hasil)) {
-  //     $output .= '<tr><td><center>'.$nomor++.'</center></td><td><center>'.$rowss['nomor_peserta'].'</center></td><td><center>'.$rowss['nama'].'</center></td><td><center>'.$rowss['total_nilai'].'</center></td>
-  //       <td><center>'.$rowss['status'].'</center></td>
-  //       <td><center>
-  //       <a href="form-edit-hasil.php?id_hasil_tes ='.$rowss['id_hasil_tes'].'"><i class="fa fa-edit"></i></a>
-  //       <a href="hapus-hasil.php?id_hasil_tes='.$rowss['id_hasil_tes'].'" onclick="return confirm ("Yakin Ingin Hapus Data Ini ?")"><i class="fa fa-trash"></i></a>
-  //       <a href="print.php?id_hasil_tes =' .$rowss['id_hasil_tes'].'"><i class="fa fa-print"></i></a></center></td>
-  //     </tr>';
-  //   }
-  //   $output .= '</tr>';
-  //   $output .= '</table>';
-  //   return $output;
-  // }
 ?>
   <div class="content-wrapper">
     <section class="content">
