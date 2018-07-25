5cabef388038b2a09bd76ae3546a8582b5d11f72
wahyu357 <cahyaniwahyu7@gmail.com>
2018-07-25 08:32:10 -0700

    a

--- kepalakantor/index.php
+++ kepalakantor/index.php
@@ -7,7 +7,7 @@
       <div class="row">
         <div class="text-center">
           <h1>Halaman Kepala Kantor <br>Selamat Datang di Sistem Informasi Eksekutif Peserta Pelatihan Kapal Pesiar Pada Disnaker ESDM Provinsi Bali</h1>
-          <p style="margin-top: 100px; margin-bottom: 250px;">
+          <p style="margin-top: 50px; margin-bottom: 20px;">
             <img src="dinasku.png" alt="DISNAKER Prov Bali" height="400px" width="400px">
           </p>
         </div>
