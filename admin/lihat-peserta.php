9535612a8fd160f4415615e9be27356aadd1b3f6
pradipta31 <pradiptadipta31@gmail.com>
2018-08-11 09:37:28 +0800

    y

--- admin/lihat-peserta.php
+++ admin/lihat-peserta.php
@@ -2,10 +2,15 @@
 include "kiri.php";
 ?>
 <?php
-  include 'koneksi.php';
   $connect = new PDO("mysql:host=localhost;dbname=sistem_informasi_eksekutif", "root", "");
+  $tahun = !empty($_GET['tahun']) ? $_GET['tahun'] : null;
 
   $query = "SELECT DISTINCT tahun FROM peserta ORDER BY tahun ASC";
+
+  $qDataTable = "SELECT * FROM peserta ORDER BY id_peserta DESC";
+  if ($tahun != null) {
+    $qDataTable = "SELECT * FROM peserta WHERE tahun='$tahun' ORDER BY id_peserta DESC";
+  }
 
   $statement = $connect->prepare($query);
 
@@ -13,11 +18,13 @@ include "kiri.php";
 
   $result = $statement->fetchAll();
 
+  $dataTable = $connect->query($qDataTable);
+
 ?>
   <div class="content-wrapper">
     <section class="content-header">
       <h1>
-      Data Peserta
+        Peserta
       </h1>
     </section>
 
@@ -30,35 +37,51 @@ include "kiri.php";
             </div>
             <!-- /.box-header -->
 
-            <input type="hidden" name="hidden_peserta" id="hidden_peserta" />
-            <label style="margin-left:20px">Pilih Tahun</label>
-            <div class="btn btn-xs">
-              <select name="multi_search_filter" id="multi_search_filter" class="form-control">
-                <option value="tampil_semua">Tampilkan Semua</option>
-               <?php
-               foreach($result as $row)
-               {
-                echo '<option value="'.$row['tahun'].'">'.$row['tahun'].'</option>';
-               }
-               ?>
-               </select>
-            </div>
-            <div class="btn btn-lg" id="btn">
-
-            </div>
-
-            <div class="table-responsive" id="table">
+            <form class="" action="" method="get" id="form_search">
+              <label style="margin-left:20px">Pilih Tahun : </label>
+              <div class="btn btn-xs">
+                <select name="tahun" id="tahun" class="form-control">
+                  <option value="">Tampilkan Semua</option>
+                  <?php foreach($result as $row) : ?>
+                    <option value="<?php echo $row["tahun"]; ?>" <?php echo $tahun == $row["tahun"] ? 'selected' : '' ?>><?php echo $row["tahun"];?></option>';
+                  <?php endforeach; ?>
+                 </select>
+              </div>
+              <div class="btn btn-lg">
+                <?php
+                echo "<a class='btn btn-primary' href='print-satu-peserta.php?tahun=".$tahun."'>Cetak Data</a>";
+                ?>
+              </div>
+            </form>
+            <div class="table-responsive">
               <table class="table table-striped table-bordered">
                <thead>
                 <tr>
-                 <th><center>No</th></center>
+                 <th><center>No</th>
                  <th><center>Nomor Peserta</th></center>
-                 <th><center>Tanggal Pendaftaran</th></center>
                  <th><center>Nama Peserta</th></center>
+                 <th><center>tgl_pendaftaran</th></center>
                  <th><center>Opsi</th></center>
                 </tr>
                </thead>
                <tbody>
+                 <?php $no = 1; ?>
+                 <?php foreach($dataTable as $hasil) : ?>
+                   <tr>
+                     <td><center><?php echo $no++; ?></center></td>
+                     <td><center><?php echo $hasil['nomor_peserta']; ?></center></td>
+                     <td><center><?php echo $hasil['nama']; ?></center></td>
+                     <td><center><?php echo $hasil['tgl_pendaftaran']; ?></center></td>
+                     <td>
+                       <center>
+                       <a href="proses-lihat-peserta.php?id_peserta=<?= $hasil['id_peserta'] ?>"><i class="fa fa-eye"></i></a>
+                       <a href="form-edit-peserta.php?id_peserta=<?= $hasil['id_peserta'] ?>"><i class="fa fa-edit"></i></a>
+                       <a href="hapus-peserta.php?id_peserta=<?= $hasil['id_peserta'] ?>" onclick = "return confirm(/Yakin Ingin hapus data ini/)"><i class="fa fa-trash"></i></a>
+                       <a href="print-semua-peserta.php?id_peserta=<?= $hasil['id_peserta'] ?>"><i class="fa fa-print"></i></a>
+                       <center>
+                     </td>
+                   </tr>
+                 <?php endforeach;?>
                </tbody>
               </table>
              <br />
@@ -71,44 +94,15 @@ include "kiri.php";
     </section>
   </div>
   <script>
-    $(document).ready(function(){
-
-     load_data();
-     loadData();
-
-     function load_data(query='')
-     {
-      $.ajax({
-       url:"tampil-peserta.php",
-       method:"POST",
-       data:{query:query},
-       success:function(data)
-       {
-        $('tbody').html(data);
-      }
-      })
-     }
-     function loadData(query='')
-     {
-      $.ajax({
-       url:"ajax.php",
-       method:"POST",
-       data:{query:query},
-       success:function(data)
-       {
-        $('#btn').html(data);
-      }
-      })
-     }
-
-     $('#multi_search_filter').change(function(){
-      $('#hidden_peserta').val($('#multi_search_filter').val());
-      var query = $('#hidden_peserta').val();
-      load_data(query);
-      loadData(query);
-     });
+  console.log("<?php echo $qDataTable; ?>");
+    $(function(){
+      $('#tahun').on('change', function(){
+        $('#form_search').submit();
+      });
     });
-</script>
+  </script>
+
+
 <?php
   include 'bawah.php';
 ?>
---
+++ admin/print-satu-peserta.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<meta charset="utf-8">
+<title></title>
+<img src="header.png" style="width:700px; height: 105px; margin-left: 30px">
+</head>
+<body>
+
+
+<div style="margin-top :12px">
+
+  <label style="margin-left : 40px">Data Calon Peserta Pelatihan Kapal Pesiar yaitu sebagai berikut : </label>
+  <table style="margin-top : 12px" width="600" border="1" align="center">
+    <tr>
+      <th><center>No</center></th>
+      <th><center>No Peserta</center></th>
+      <th><center>Nama</center></th>
+    </tr>
+    <?php
+    $tahun = !empty($_GET['tahun']) ? $_GET['tahun'] : null;
+    $qDataTable = "SELECT * FROM peserta ORDER BY id_peserta DESC";
+    if ($tahun != null) {
+      $qDataTable = "SELECT * FROM peserta WHERE tahun='$tahun' ORDER BY id_peserta DESC";
+    }
+  include "koneksi.php";
+  $no = 1;
+  $query = mysqli_query($koneksi, $qDataTable);
+  while ($result = mysqli_fetch_assoc($query)) {
+  ?>
+          <tr>
+            <td><center><?php echo $no++; ?></center></td>
+              <td><center><?php echo $result['nomor_peserta']; ?></center></td>
+              <td><center><?php echo $result['nama']; ?></center></td>
+            </tr>
+            <?php
+            }
+        ?>
+  </table>
+  </div>
+
+</body>
+</html>
+
+<?php
+//otomatis muncul ketika laman di akses
+echo "<script>window.print()</script>";
+?>
